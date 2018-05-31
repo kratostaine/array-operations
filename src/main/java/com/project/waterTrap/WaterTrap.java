4067fe3f923400534d282544036965122ee9206a
kratostaine <Madhu.bhat.8j@gmail.com>
2018-05-31 16:37:33 +0530

    modified variable name

--- src/main/java/com/project/waterTrap/WaterTrap.java
+++ src/main/java/com/project/waterTrap/WaterTrap.java
@@ -4,23 +4,23 @@ public class WaterTrap {
 
 	public static int getWaterTrapped(int[] towers) {
 		int n = towers.length;
-		int lIndex = 0, rIndex = n - 1;
+		int lIndex = 0, hIndex = n - 1;
 		int lMax = 0, rMax = 0;
 		int result = 0;
 
-		while (lIndex <= rIndex) {
-			if (towers[lIndex] < towers[rIndex]) {
+		while (lIndex <= hIndex) {
+			if (towers[lIndex] < towers[hIndex]) {
 				if (towers[lIndex] > lMax)
 					lMax = towers[lIndex];
 				else
 					result += lMax - towers[lIndex];
 				lIndex++;
 			} else {
-				if (towers[rIndex] > rMax)
-					rMax = towers[rIndex];
+				if (towers[hIndex] > rMax)
+					rMax = towers[hIndex];
 				else
-					result += rMax - towers[rIndex];
-				rIndex--;
+					result += rMax - towers[hIndex];
+				hIndex--;
 			}
 		}
 		return result;
